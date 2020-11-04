a2141f51265a343cd51ee7ee1cc7bfd4db369de0
Denis Xelizait <70703751+Xelizait@users.noreply.github.com>
2020-11-04 19:51:49 +0300

    lab6 start

---
+++ 2nd year/programming/лаба6/лаба6/Program.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using lan6;
+
+namespace lab5
+{
+    interface IActionable
+    {
+        void Resize();
+        void Show();
+        void Input();
+        void Move();
+    }
+
+    interface IAlive
+    {
+        void Clone();
+    }
+
+    public abstract class Figure : IActionable
+    {
+        protected string figurename;
+        protected int width;
+        protected int heigth;
+        protected string color;
+        protected int id;
+        protected int figureArea;
+
+        public int FigureArea
+        {
+            get { return figureArea; }
+            set { figureArea = value; }
+        }
+
+        public string Figurename
+        {
+            get { return figurename; }
+            set { figurename = value; }
+        }
+
+        public int Width
+        {
+            get { return width; }
+            set { width = value; }
+        }
+
+        public int Heigth
+        {
+            get { return heigth; }
+            set { heigth = value; }
+        }
+
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public override string ToString()
+        {
+            Console.WriteLine("Фигура: " + Figurename);
+            Console.WriteLine("Ширина: " + Width);
+            Console.WriteLine("Высота: " + Heigth);
+            Console.WriteLine("Цвет: " + Color);
+            Console.WriteLine("ID: " + Id);
+            return " тип " + base.ToString();
+        }
+
+        virtual public bool ChangeWeapon(string newWeapon)
+        {
+            return true;
+        }
+        public abstract void Resize();
+        public abstract void Show();
+        public abstract void Input();
+        public abstract void Move();
+        public abstract void Clone();
+    }
+
+    public class Ellipse : Figure, IAlive
+    {
+        public Ellipse(string figurename, int id, int heigth, string color)
+        {
+            this.figurename = figurename;
+            this.id = id;
+            this.heigth = heigth;
+            this.color = color;
+        }
+        override public void Move() // реализуем методы из интерфейса
+        {
+            Console.WriteLine("Произошло передвижение фигуры!!!");
+        }
+
+        public override void Show()
+        {
+            Console.WriteLine("Фигура показалась!");
+        }
+
+        public override void Resize()
+        {
+            Console.WriteLine("Изменяем размер");
+            heigth = 123;
+            width = 123;
+        }
+        public override void Input()
+        {
+            Console.WriteLine("Вставка фигуры!");
+        }
+
+        public override void Clone() // метод из абстрактного класса 
+        {
+            Console.WriteLine("МЕТОД АБСТРАКТНОГО КЛАССА");
+        }
+
+        void IAlive.Clone()   // метод из интерсфейса 
+        {
+            Console.WriteLine("МЕТОД ИНТЕРФЕЙСА ОГО");
+        }
+    }
+    public class Rectangle : Figure, IAlive
+    {
+        public Rectangle(string name, int id, int height, string color, int width)
+        {
+            this.figurename = name;
+            this.id = id;
+            this.heigth = height;
+            this.color = color;
+            this.width = width;
+        }
+
+        public override void Clone()
+        {
+            Console.WriteLine("МЕТОД АБСТРАКТНОГО КЛАССА");
+        }
+        void IAlive.Clone()
+        {
+            Console.WriteLine("МЕТОД ИНТЕРФЕЙСА ОГО");
+        }
+        override public void Move()
+        {
+            Console.WriteLine("Произошло перемещение фигуры!!!");
+        }
+        public override void Show()
+        {
+            Console.WriteLine("Фигура показалась!");
+        }
+        public override void Resize()
+        {
+            Console.WriteLine("Изменяем размер");
+            heigth = 123;
+            width = 123;
+        }
+        public override void Input()
+        {
+            Console.WriteLine("Вставка фигуры!");
+        }
+
+    }
+
+    sealed public class Button : Rectangle
+    {
+        public bool PushedOrNot;
+        public Button(string name, int id, int height, string color, int width, bool PushedOrNot) : base(name, id, height, color, width)
+        {
+            this.PushedOrNot = PushedOrNot;
+        }
+        public bool ToCheck()
+        {
+            if (PushedOrNot)
+                return true;
+            else
+                return false;
+        }
+    }
+
+    sealed public class Checkbox : Rectangle
+    {
+        public bool PushedOrNot;
+        public Checkbox(string name, int id, int height, string color, int width, bool PushedOrNot) : base(name, id, height, color, width)
+        {
+            this.PushedOrNot = PushedOrNot;
+        }
+        public bool ToCheck()
+        {
+            if (PushedOrNot)
+                return true;
+            else
+                return false;
+        }
+    }
+
+    sealed public class Radiobutton : Ellipse
+    {
+        public bool PushedOrNot;
+        public Radiobutton(string name, int id, int height, string color, bool PushedOrNot) : base(name, id, height, color)
+        {
+            this.PushedOrNot = PushedOrNot;
+        }
+        public bool ToCheck()
+        {
+            if (PushedOrNot)
+                return true;
+            else
+                return false;
+        }
+    }
+
+    public static class GenFigure<Type> where Type : Figure
+    {
+
+        public static void Func(Type figure)
+        {
+            Console.WriteLine($"Фигура: {figure.Figurename}");
+            Console.WriteLine($"Ширина: {figure.Width}");
+            Console.WriteLine($"Высота: {figure.Heigth}");
+        }
+
+    }
+
+    public class forObject : Object // переопределение методов Object
+    {
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            if (this.GetType() != obj.GetType()) return false;
+            return true;
+        }
+
+        public string Name { get; set; }
+        int sNumber;
+
+        public override int GetHashCode()
+        {
+            int hash = 269;
+            hash = string.IsNullOrEmpty(Name) ? 0 : Name.GetHashCode();
+            hash = (hash * 47) + sNumber.GetHashCode();
+            return hash;
+        }
+
+    }
+
+    public class Printer
+    {
+        public void IAmPrinting(Figure figure)
+        {
+            Console.WriteLine(figure.ToString());
+        }
+    }
+
+
+
+    }
+

b5d77abc01d4d215bf752355840e8577f19873f8
Denis Xelizait <70703751+Xelizait@users.noreply.github.com>
2020-11-04 19:58:42 +0300

    class controller

---
+++ 2nd year/programming/лаба6/лаба6/Controller.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using lab5;
+using lan6;
+
+namespace lan6
+{
+  
+
+     partial class Controller
+    {
+        
+        public static int FindTheBiggest(List<Figure> figures)
+        {
+            int biggest, index = 0;
+            biggest = figures[0].FigureArea;
+            for (int i = 1; i < figures.Count; i++)
+            {
+                if (biggest < figures[i].FigureArea)
+                {
+                    biggest = figures[i].FigureArea;
+                    index = i;
+                }
+            }
+            return index;    
+        }
+
+        public static int FindTotal(List<Figure> figures)
+        {
+            int total = 0;
+            total = figures[0].FigureArea;
+            for (int i = 1; i < figures.Count; i++)
+            {
+                total = total + figures[i].FigureArea;                
+            }
+            return total;
+        }
+
+
+
+    }
+}

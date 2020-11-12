05bdbbb3d7bd5bf9c70c99cb6b12e2915a64cd1e
Denis Xelizait <70703751+Xelizait@users.noreply.github.com>
2020-11-12 18:17:21 +0300

    lab7

---
+++ 2nd year/programming/лаба7/lan6/Class2.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using lab5;
+
+
+namespace lan6
+{
+
+
+    class WrongIdValue : Exception
+    {
+        int Value { get; set; }
+        public WrongIdValue(string message, int value) : base(message)
+        {
+            Value = value;
+
+        }
+    }
+
+    class WrongColorValue : ArgumentOutOfRangeException
+    {
+        int Value { get; set; }
+        public WrongColorValue(string message, int value) : base(message)
+        {
+            Value = value;
+
+        }
+    }
+
+
+    class IsNotFigure : ArgumentException
+    {
+        string Value { get; set; }
+        public IsNotFigure(string message, string value) : base(message)
+        {
+            Value = value;
+
+        }
+    }
+
+    class IsNotName : ArgumentException
+    {
+        string Value { get; set; }
+        public IsNotName(string message, string value) : base(message)
+        {
+            Value = value;
+
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+   
+
---
+++ 2nd year/programming/лаба7/lan6/Class3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using lab5;
+
+namespace lab5
+{
+    public class WrongIdValue : Exception
+    {
+        public WrongIdValue(string message) : base(message)
+        {
+
+
+        }
+    }
+}
